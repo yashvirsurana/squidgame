0b96c06fd1b132ea838a73a993fab7df044920aa
Yashvir Surana <yashvirsurana@yashvirs-mbp.home>
2022-01-17 18:50:50 +0000

    20000 secs

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pio.templates.default = "plotly_dark"
 
 # Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
 # after it's been refreshed 100 times.
-count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
+count = st_autorefresh(interval=2000, limit=10000, key="fizzbuzzcounter")
 
 # The function returns a counter for number of refreshes. This allows the
 # ability to make special requests at different intervals based on the count

9e2d9772817624f33717e06e01c1310bad7f00f8
yashvirsurana <yashvirsurana@gmail.com>
2022-01-21 12:59:09 +0000

    @

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import plotly.graph_objects as go
 from PIL import Image
 import base64
 import random
+import utils as utl
+
+utl.local_css("stylesheet.css")
 
 ENV=open(".streamlit/config.toml").read().splitlines()[1].split('=')[1].strip('\"')
 
@@ -126,29 +129,7 @@ with col2:
 
     st.plotly_chart(give_fig(a.c,a.name), use_container_width=True)
 
-st.markdown(
-        """
-        <style>
 
-        @font-face {
-        font-family: 'Game Of Squids';
-        src: url(../squidgame/GameOfSquids.ttf),
-             url(../squidgame/GameOfSquids.woff2),
-             url(../squidgame/GameOfSquids.woff);
-        font-weight: normal;
-        font-style: normal;
-        font-display: swap;
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-
-        }
-   
-        html, body, [class*="css"]  {
-        font-family: 'Game Of Squids';
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
 quote = open('quotes.txt').read().splitlines()
 num = (str(count)[-2])

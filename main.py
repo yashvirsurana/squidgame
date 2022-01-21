a3d272327391d844afc9087d7d212216140e045f
yashvirsurana <yashvirsurana@gmail.com>
2022-01-21 13:12:12 +0000

    asd

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import plotly.graph_objects as go
 from PIL import Image
 import base64
 import random
-import utils as utl
-
-utl.local_css("stylesheet.css")
 
 ENV=open(".streamlit/config.toml").read().splitlines()[1].split('=')[1].strip('\"')
 
@@ -129,7 +126,25 @@ with col2:
 
     st.plotly_chart(give_fig(a.c,a.name), use_container_width=True)
 
+st.markdown(
+        """
+        <style>
+        @font-face {
+          font-family: 'squid';
+          font-style: normal;
+          font-weight: 400;
+          src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
 
+        html, body, [class*="css"]  {
+        font-family: 'squid';
+        }
+        </style>
+
+    """,
+        unsafe_allow_html=True,
+    )
 
 quote = open('quotes.txt').read().splitlines()
 num = (str(count)[-2])
